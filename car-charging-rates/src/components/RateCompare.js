82bc7edf6e3b92f431a5d9e52ebc190e94407bd9
kjones <jones.kjg@gmail.com>
2021-04-10 13:13:46 -0400

    Add monthly cost and potential savings info

--- car-charging-rates/src/components/RateCompare.js
+++ car-charging-rates/src/components/RateCompare.js
@@ -8,19 +8,31 @@ class RateCompare extends Component {
     render() {
         const { calc } = this.props
         console.log("Calc object upon loading results: ", calc)
-        const formattedHomeData = Object.keys(calc.altHomes).map((key) => {
+        const { altEvs, altHomes, yearlyEv, yearlyHome } = calc
+        const formattedHomeData = Object.keys(altHomes).map((key) => {
             return {
-                y: calc.altHomes[key], label: key
+                y: altHomes[key], label: key
             }
-        }).concat({y: Object.values(calc.yearlyHome)[0], label: `${Object.keys(calc.yearlyHome)[0]}(Your chosen rate)`})
+        }).concat({y: Object.values(yearlyHome)[0], label: `${Object.keys(yearlyHome)[0]}(Your chosen rate)`})
 
-        const formattedEvData = Object.keys(calc.altEvs).map((key) => {
+        const formattedEvData = Object.keys(altEvs).map((key) => {
             return {
-                y: calc.altEvs[key], label: key
+                y: altEvs[key], label: key
             }
-        }).concat({y: Object.values(calc.yearlyEv)[0], label: `${Object.keys(calc.yearlyEv)[0]}(Your chosen rate)`})
+        }).concat({y: Object.values(yearlyEv)[0], label: `${Object.keys(yearlyEv)[0]}(Your chosen rate)`})
 
-        console.log(formattedHomeData, formattedEvData)
+        let altTotals = {}
+
+        Object.keys(altHomes).forEach((key) => {
+            altTotals[key] = altHomes[key] + altEvs[key]
+        })
+
+                                                                                                                          
+        const minAltCost = Math.min(...Object.values(altTotals))
+        const cheapestAltRate = Object.keys(altTotals).find((key) => altTotals[key] === minAltCost)
+
+        const cost = yearlyEv + yearlyHome
+
         const options = {
             title: {
                 text: "Yearly Costs Compared"
@@ -42,19 +54,28 @@ class RateCompare extends Component {
 
         }
         return(
-            <div>
-                {this.props.loading === true
-                ? null
-                : <CanvasJSChart options={options}/>
-                }   
+            <div className='rate-compare-main'>
+                <div className='rate-compare-chart'>
+                    {this.props.loading === true
+                    ? null
+                    : <CanvasJSChart options={options}/>
+                    }  
+                </div>
+                <div className='rate-compare-text'>
+                    <p>{`At your current rate, your total electrical bill would be $${cost/12} monthly`}</p>
+                    {cost > minAltCost 
+                    ? <p>{`You could save $${(cost - minAltCost)/12} monthly by switching to ${cheapestAltRate}(${this.props.rates[cheapestAltRate].description})`}</p>
+                    : <p>{`This plan will save you at least $${minAltCost - cost} monthly`}</p>}    
+                </div> 
             </div>
         )
     }
 }
 
-function mapStateToProps({ calc }){
+function mapStateToProps({ calc, rates }){
     return {
         calc,
+        rates,
         loading: Object.keys(calc).length === 0
     }
 }
