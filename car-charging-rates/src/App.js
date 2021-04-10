024254bd58d6ba0b994785d02bb90b645aeaa6e9
kjones <jones.kjg@gmail.com>
2021-04-09 23:50:46 -0400

    wrap main app in another div

--- car-charging-rates/src/App.js
+++ car-charging-rates/src/App.js
@@ -2,7 +2,6 @@ import { Component, Fragment } from 'react';
 import './App.css';
 import InputForm from './components/InputForm'
 import RateCompare from './components/RateCompare'
-import { getRateOptions } from './utils/_DATA'
 import { handleReceiveRates } from './actions/rates'
 import { BrowserRouter as Router, Route, } from 'react-router-dom' 
 import LoadingBar from 'react-redux-loading-bar'
@@ -19,13 +18,15 @@ class App extends Component {
       <Router>
         <Fragment>
           <LoadingBar />
-          {this.props.loading === true 
-          ? null
-          : <div className='App'>
-              <Route exact path ='/' component={InputForm}/>
-              <Route exact path='/results' component={RateCompare}/>
-            </div>
-          }
+          <div>
+            {this.props.loading === true 
+            ? null
+            : <div className='App'>
+                <Route exact path ='/' component={InputForm}/>
+                <Route exact path='/results' component={RateCompare}/>
+              </div>
+            }
+          </div>
         </Fragment>
       </Router>
     );
