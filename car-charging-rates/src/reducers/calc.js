5a222929bd6aaac4583f0df97dfadbcbb8cc365d
kjones <jones.kjg@gmail.com>
2021-04-10 00:45:58 -0400

    slight change to calc reducer

--- car-charging-rates/src/reducers/calc.js
+++ car-charging-rates/src/reducers/calc.js
@@ -3,7 +3,10 @@ import { CALC_RATES } from '../actions/calc'
 export default function calc(state = {}, action) {
     switch (action.type){
         case CALC_RATES:
-            return action.calcs
+            return {
+                ...state,
+                ...action.calcs
+            }
         default:
             return state
     }
