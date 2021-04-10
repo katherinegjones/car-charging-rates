a2afefd72bd12d2edb4302becd726dff0191bf16
kjones <jones.kjg@gmail.com>
2021-04-10 00:44:25 -0400

    slight change to reducers

--- car-charging-rates/src/reducers/rates.js
+++ car-charging-rates/src/reducers/rates.js
@@ -3,7 +3,10 @@ import { RECEIVE_RATES, ADD_RATE } from '../actions/rates'
 export default function rates(state={}, action) {
     switch (action.type){
         case RECEIVE_RATES:
-            return action.rateOptions
+            return {
+                ...state,
+                ...action.rateOptions
+            }
         case ADD_RATE:
             return {
                 ...state,
